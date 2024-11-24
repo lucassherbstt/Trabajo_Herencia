f03bb4d64822920555904ed3a2edaee1646736da
lucassherbstt <lucasherbst82@gmail.com>
2024-11-24 13:59:34 -0500

    Create CorreoElectronico.java

---
+++ Ejercicio 3 /CorreoElectronico.java
@@ -0,0 +1,25 @@
+package Ejercicio3;
+
+public class CorreoElectronico extends CanalNotificacion implements Personalizable {
+    private String direccionCorreo;
+
+    public CorreoElectronico(String usuario, String mensaje, String direccionCorreo) {
+        super(usuario, mensaje);
+        this.direccionCorreo = direccionCorreo;
+    }
+
+    @Override
+    public void enviarNotificacion() {
+        System.out.println("Enviando correo a " + direccionCorreo + ": " + mensaje);
+    }
+
+    @Override
+    public void personalizarMensaje(String nuevoMensaje) {
+        this.mensaje = nuevoMensaje;
+    }
+
+    @Override
+    public String getInfoCanal() {
+        return super.getInfoCanal() + " (Correo electrónico a: " + direccionCorreo + ")";
+    }
+}
